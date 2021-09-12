175dd123f60ca1770d4644c23a7fe742cfae99c7
Simon Wächter <waechter.simon@gmail.com>
2021-09-12 15:27:46 +0200

    Externalize the configuration

--- src/web/src/main/java/ch/swaechter/libreshare/web/Application.java
+++ src/web/src/main/java/ch/swaechter/libreshare/web/Application.java
@@ -1,15 +1,18 @@
 package ch.swaechter.libreshare.web;
 
+import ch.swaechter.libreshare.web.utils.globals.Globals;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+import java.io.File;
+
 @OpenAPIDefinition(
     info = @Info(
         title = "libreshare",
-        version = "0.0.1"
+        version = Globals.LIBRESHARE_VERSION
     )
 )
 @SecurityScheme(name = "default",
@@ -21,6 +24,23 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 public class Application {
 
     public static void main(String[] arguments) {
+        // Check the command arguments
+        if (arguments.length != 1) {
+            System.err.println("Usage:   java -jar web.jar <Path to settings JSON file>");
+            System.err.println("Example: java -jar web.jar config/application.json");
+            System.exit(1);
+        }
+
+        // Check if the settings file exist
+        File settingsJsonFile = new File(arguments[0]);
+        if (!settingsJsonFile.isFile()) {
+            System.err.println("The file path " + settingsJsonFile.getAbsolutePath() + " is not a file");
+            System.exit(1);
+        }
+
+        // Set the system property
+        System.setProperty("micronaut.config.files", settingsJsonFile.getAbsolutePath());
+
         // Launch the application
         Micronaut.build(arguments).mainClass(Application.class).banner(false).start();
     }
---
+++ src/web/src/main/java/ch/swaechter/libreshare/web/utils/globals/Globals.java
@@ -0,0 +1,10 @@
+package ch.swaechter.libreshare.web.utils.globals;
+
+public class Globals {
+
+    public static final String LIBRESHARE_VERSION = "0.0.1";
+
+    // Private constructor
+    private Globals() {
+    }
+}
